f0144f9166e372ae0056ffcf1d5a02a5d0c620cf
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-18 02:24:23 -0400

    Create replace-vars.js

---
+++ assets/replace-vars.js
@@ -0,0 +1,18 @@
+  // Replacing Variables At Top Of This Document
+  
+  var GuestBook = document.getElementById("GuestBook") ;
+GuestBook.innerHTML = GuestBook.innerHTML
+                        .replace(/GOOGLE_ENTRY_ID_Name/g, GOOGLE_ENTRY_ID_Name)
+	
+	.replace(/GOOGLE_ENTRY_ID_Email/g, GOOGLE_ENTRY_ID_Email)
+	
+	.replace(/GOOGLE_ENTRY_ID_TextArea/g, GOOGLE_ENTRY_ID_Guestbook)
+	
+
+	
+	
+	
+	
+	
+                         .replace(/GOOGLE_FORM_URL/g, Google_Form_Link)
+  ;
